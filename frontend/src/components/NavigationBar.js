0ed682ea1a080f5a5364781cb63672c3333ec31b
Omar Ashraf <omarashraf@Omars-MacBook-Air.local>
2022-10-27 00:34:42 +0200

    frontend inital setup

---
+++ frontend/src/components/NavigationBar.js
@@ -0,0 +1,20 @@
+const NavigationBar = () =>{
+    return (
+        <header>
+            <div className="container">
+                <Link to ="/">
+                    <h1>
+                        Workout Buddy
+                    </h1>
+                </Link>
+
+            </div>
+
+        </header>
+       
+
+
+    )
+}
+
+export default NavigationBar
