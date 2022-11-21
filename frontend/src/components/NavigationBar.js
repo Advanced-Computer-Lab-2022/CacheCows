1a5c6eceefc8dae7bd5f7d8a096a91d6b14f7987
Omar Aboelazm <omarashraff22@gmail.com>
2022-11-21 16:16:27 +0200

    user data retrieved from local storage when refreshing page, and navigation bar corresponds to current status either logged in or logged out

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -23,7 +23,7 @@ const NavigationBar = () =>{
                     </h1>
                 </Link>
                 <Link to="/"> Home </Link>
-                <Link to="/about"> About </Link>
+                <Link to="/about"> About Us </Link>
 
          <nav>
                  {user && (
