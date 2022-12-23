e055ccc5f1e185e537eb8690a203a342393c2d50
Omar Aboelazm <omarashraff22@gmail.com>
2022-12-23 19:49:53 +0200

    login page beautified

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { Link } from 'react-router-dom'
+import Box from '@mui/material/Box';
+import instructor from '../assets/teacher2.png';
+import indtrainee from '../assets/indtrainee.png';
+import corptrainee from '../assets/corporate.png';
+import admin from '../assets/admin.png';
 
 
 function Signup() {
@@ -12,50 +18,105 @@ const navigate=useNavigate();
     <div>
       
       <div>
-      <button
-        onClick={() => {
-          navigate("/InstLogin");
-        }}
-      >
-        {" "}
-        Login as Instructor!
+
+      <button>
+        <Link onClick={() => window.location.href=`/InstLogin`}>
+                <Box
+                  component="img"
+                  sx={{ height: 200, width: 200 , padding : 0, margins: 0, 
+                    backgroundColor: '#1c4c64',
+                    '&:hover': {
+                      backgroundColor: '#141415',
+                      opacity: [0.9, 0.8, 0.7],
+                    },
+                  }}
+                  alt="Logo"
+                  src={instructor}
+                />
+              </Link>
+              <br />
+              Instructor
+              
+      </button>
+
+      &nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;
+
+
+      <button>
+        <Link onClick={() => window.location.href=`/IndTraineeLogin`}>
+                <Box
+                  component="img"
+                  sx={{ height: 200, width: 200 , padding : 0, margins: 0, 
+                    backgroundColor: '#1c4c64',
+                    '&:hover': {
+                      backgroundColor: '#141415',
+                      opacity: [0.9, 0.8, 0.7],
+                    },
+                  }}
+                  alt="Logo"
+                  src={indtrainee}
+                />
+              </Link>
+              <br />
+              Individual Trainee
+      </button>
+
+          &nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;
+
+
+
+      <button>
+        <Link onClick={() => window.location.href=`/CorpTraineeLogin`}>
+                <Box
+                  component="img"
+                  sx={{ height: 200, width: 200 , padding : 0, margins: 0, 
+                    backgroundColor: '#1c4c64',
+                    '&:hover': {
+                      backgroundColor: '#141415',
+                      opacity: [0.9, 0.8, 0.7],
+                    },
+                  }}
+                  alt="Logo"
+                  src={corptrainee}
+                />
+              </Link>
+              <br />
+              Corporate Trainee
+      </button>
+
+      &nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;
+
+
+
+      <button>
+        <Link onClick={() => window.location.href=`/AdminLogin`}>
+                <Box
+                  component="img"
+                  sx={{ height: 200, width: 200 , padding : 0, margins: 0, 
+                    backgroundColor: '#1c4c64',
+                    '&:hover': {
+                      backgroundColor: '#141415',
+                      opacity: [0.9, 0.8, 0.7],
+                    },
+                  }}
+                  alt="Logo"
+                  src={admin}
+                />
+              </Link>
+              <br />
+              Admin
       </button>
       </div>
       <br />
 
-      <div>
-      <button
-        onClick={() => {
-          navigate("/IndTraineeLogin");
-        }}
-      >
-        {" "}
-        Log in as Individual Trainee!
-      </button>
-      </div>
-      <br />
-
-      <div>
-      <button
-        onClick={() => {
-          navigate("/AdminLogin");
-        }}
-      >
-        {" "}
-        Log in as Admin!
-      </button>
-      </div>
-      <br />
-      <div>
-      <button
-        onClick={() => {
-          navigate("/CorpTraineeLogin");
-        }}
-      >
-        {" "}
-        Login as Corporate Trainee!
-      </button>
-      </div>
+      
+      
       <br />
       <div>
       <button
@@ -64,12 +125,24 @@ const navigate=useNavigate();
         }}
       >
         {" "}
-        New User ? Sign Up Now!
+        New User ? 
+        <br></br>
+        Sign Up Now!
       </button>
       </div>
       <br />
 
       <div>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+
       <button
         onClick={() => {
           navigate("/");
@@ -82,10 +155,6 @@ const navigate=useNavigate();
 
 
       
-      
-
-      
-
     </div>
   );
 }
